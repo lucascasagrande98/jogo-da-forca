84a0ad7be6acf5137741ec56f3ae30cb4a3e9de9
lucascasagrande98 <lucascasagrande98@gmail.com>
2023-02-21 20:01:55 -0300

    projeto concluido

---
+++ js/jogo.js
@@ -0,0 +1,70 @@
+const criaJogo = (sprite) => {
+	let etapa = 1;
+	let lacunas = [];
+	let palavraSecreta = "";
+
+	const criaLacunas = () => {
+		for (let i = 0; i < palavraSecreta.length; i++) {
+			lacunas.push("");
+		}
+		//lacunas = Array(palavraSecreta.length).fill("");
+	};
+
+	const proximaEtapa = () => (etapa = 2);
+
+	const setPalavraSecreta = (palavra) => {
+		if (!palavra.trim()) throw Error("Palavra secreta inválida!");
+		palavraSecreta = palavra;
+		criaLacunas();
+		proximaEtapa();
+	};
+
+	const getLacunas = () => lacunas;
+
+	const getEtapa = () => etapa;
+
+	const processaChute = (chute) => {
+		if (!chute.trim()) throw Error("Chute inválido");
+
+		const exp = new RegExp(chute, "gi");
+		let resultado,
+			acertou = false;
+
+		while ((resultado = exp.exec(palavraSecreta))) {
+			acertou = lacunas[resultado.index] = chute;
+		}
+
+		if (!acertou) {
+			sprite.nextFrame();
+		}
+	};
+
+	const ganhou = () =>
+		lacunas.length
+			? !lacunas.some((lacuna) => {
+					return lacuna == "";
+			  })
+			: false;
+
+	const perdeu = () => sprite.isFinished();
+
+	const ganhouOuPerdeu = () => ganhou() || perdeu();
+
+	const reinicia = () => {
+		etapa = 1;
+		palavraSecreta = "";
+		lacunas = [];
+		sprite.reset();
+	};
+
+	return {
+		setPalavraSecreta,
+		getLacunas,
+		getEtapa,
+		processaChute,
+		ganhou,
+		perdeu,
+		ganhouOuPerdeu,
+		reinicia,
+	};
+};
